c26ae9285bf86ae3f553bfcac0731a17dbf6f683
Kaleb MacArthur <kmaca3@ocdsb.ca>
2023-03-21 09:53:30 -0400

    debug

--- app/src/main/java/com/example/maplelogger/MainActivity.java
+++ app/src/main/java/com/example/maplelogger/MainActivity.java
@@ -103,8 +103,8 @@ public class MainActivity extends AppCompatActivity {
 
                 assert doc != null;
                 System.out.println(doc.getFirstChild().getNodeName());
-                System.out.println(doc.getFirstChild().toString());
-                Snackbar.make(view, doc.getElementById("currentConditions").getAttribute("temperature"), Snackbar.LENGTH_LONG)
+                System.out.println(doc.getFirstChild().getAttributes().item(5).getNodeName());
+                Snackbar.make(view, doc.getFirstChild().getNodeName(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
