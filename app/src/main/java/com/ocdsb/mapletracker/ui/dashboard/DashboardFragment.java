e4040f16a2edf95f7ab97873d020f29b9278a788
Zachary Backmeier <backmeier.zachary@gmail.com>
2023-04-06 14:35:05 -0400

    added imports for OSM

--- app/src/main/java/com/ocdsb/mapletracker/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/ocdsb/mapletracker/ui/dashboard/DashboardFragment.java
@@ -5,6 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+// imports required for OSM
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
+import androidx.appcompat.app.AppCompatActivity; //unsure if needed
+import androidx.core.app.ActivityCompat; //unsure if needed
+import androidx.core.content.ContextCompat;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
