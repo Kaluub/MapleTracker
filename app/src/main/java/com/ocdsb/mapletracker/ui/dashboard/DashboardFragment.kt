198089a70c309f104c52602f01d8ff1f0b72bd66
Zachary Backmeier <backmeier.zachary@gmail.com>
2023-04-07 09:32:50 -0400

    Converted DashboardFragment to kotlin

--- app/src/main/java/com/ocdsb/mapletracker/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/ocdsb/mapletracker/ui/dashboard/DashboardFragment.kt
@@ -1,54 +1,49 @@
-package com.ocdsb.mapletracker.ui.dashboard;
+package com.ocdsb.mapletracker.ui.dashboard
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.ocdsb.mapletracker.R
+import com.ocdsb.mapletracker.databinding.FragmentDashboardBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.views.MapView
+
 // imports required for OSM
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.preference.PreferenceManager;
-import androidx.appcompat.app.AppCompatActivity; //unsure if needed
-import androidx.core.app.ActivityCompat; //unsure if needed
-import androidx.core.content.ContextCompat;
-import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.views.MapView;
-import java.util.ArrayList;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.ocdsb.mapletracker.MainActivity;
-import com.ocdsb.mapletracker.R;
-import com.ocdsb.mapletracker.databinding.FragmentDashboardBinding;
-import java.util.ArrayList;
-
-public class DashboardFragment extends Fragment {
-
-    private FragmentDashboardBinding binding;
-    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
-    private MapView map = null;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//unsure if needed
+//unsure if needed
+class DashboardFragment : Fragment() {
+    private var binding: FragmentDashboardBinding? = null
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    private var map: MapView? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val dashboardViewModel = ViewModelProvider(this).get(
+            DashboardViewModel::class.java
+        )
+        binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding!!.root
+        val textView = binding!!.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) { text: CharSequence? ->
+            textView.text = text
+        }
 
         //handle permissions first, before map is created. not depicted here
 
         //load/initialize the osmdroid configuration, this can be done
-        Context ctx = getActivity().getApplicationContext(); //unsure how to fix error method should exist from android.content.Context
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        val ctx =
+            activity!!.applicationContext //unsure how to fix error method should exist from android.content.Context
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
         //setting this before the layout is inflated is a good idea
         //it 'should' ensure that the map has a writable location for the map cache, even without permissions
         //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
@@ -57,63 +52,66 @@ public class DashboardFragment extends Fragment {
         //tile servers will get you banned based on this string
         //inflate and create the map
         //setContentView(R.layout.activity_main); //this line probably doesn't need to exist as this is not the main activity
-        map = (MapView) getView().findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-
-       requestPermissionsIfNecessary(arrayOf( //arrayOf does not exist in Java, can be fixed by converting file to kotlin or finding another method
+        map = view!!.findViewById<View>(R.id.map) as MapView
+        map!!.setTileSource(TileSourceFactory.MAPNIK)
+        requestPermissionsIfNecessary(
+            arrayOf( //arrayOf does not exist in Java, can be fixed by converting file to kotlin or finding another method
                 // if you need to show the current location, uncomment the line below
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                // WRITE_EXTERNAL_STORAGE is required in order to show the map
+                Manifest.permission.ACCESS_FINE_LOCATION,  // WRITE_EXTERNAL_STORAGE is required in order to show the map
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ));
-        return root;
+            )
+        )
+        return root
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
-
-    @Override
-    public void onPause() {
-        super.onPause();
+    override fun onPause() {
+        super.onPause()
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
-        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+        map!!.onPause() //needed for compass, my location overlays, v6.0.0 and up
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (int i = 0; i < grantResults.length; i++) {
-            permissionsToRequest.add(permissions[i]);
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        val permissionsToRequest = ArrayList<String>()
+        for (i in grantResults.indices) {
+            permissionsToRequest.add(permissions[i])
         }
-        if (permissionsToRequest.size() > 0) {
+        if (permissionsToRequest.size > 0) {
             ActivityCompat.requestPermissions(
-                    getActivity(),
-                    permissionsToRequest.toArray(new String[0]),
-                    REQUEST_PERMISSIONS_REQUEST_CODE);
+                activity!!,
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE
+            )
         }
     }
 
-    private void requestPermissionsIfNecessary(String[] permissions) {
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(getActivity(), permission)
-                    != PackageManager.PERMISSION_GRANTED) {
+    private fun requestPermissionsIfNecessary(permissions: Array<String>) {
+        val permissionsToRequest = ArrayList<String>()
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(activity!!, permission)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
                 // Permission is not granted
-                permissionsToRequest.add(permission);
+                permissionsToRequest.add(permission)
             }
         }
-        if (permissionsToRequest.size() > 0) {
+        if (permissionsToRequest.size > 0) {
             ActivityCompat.requestPermissions(
-                    getActivity(),
-                    permissionsToRequest.toArray(new String[0]),
-                    REQUEST_PERMISSIONS_REQUEST_CODE);
+                activity!!,
+                permissionsToRequest.toTypedArray(),
+                REQUEST_PERMISSIONS_REQUEST_CODE
+            )
         }
     }
 }
