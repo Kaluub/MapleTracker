cfedcb107051e0ed3f8c1fdc33c2d33604c31b1d
Zachary Backmeier <backmeier.zachary@gmail.com>
2023-04-19 09:13:24 -0400

    Add map into the new tree fragment

--- app/src/main/java/com/ocdsb/mapletracker/ui/management/NewTreeFragment.java
+++ app/src/main/java/com/ocdsb/mapletracker/ui/management/NewTreeFragment.java
@@ -1,7 +1,13 @@
 package com.ocdsb.mapletracker.ui.management;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,25 +15,147 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.button.MaterialButton;
+import com.ocdsb.mapletracker.Config;
 import com.ocdsb.mapletracker.R;
+import com.ocdsb.mapletracker.api.MapAPI;
+import com.ocdsb.mapletracker.databinding.FragmentManagementBinding;
+import com.ocdsb.mapletracker.databinding.FragmentNewTreeBinding;
 
-public class NewTreeFragment extends Fragment {
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Marker;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class NewTreeFragment extends Fragment implements MapEventsReceiver {
+    private FragmentNewTreeBinding binding;
+    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+    private MapView map = null;
+    private final ArrayList<GeoPoint> lookup = new ArrayList<>();
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_new_tree, container, false);
+        binding = FragmentNewTreeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        Context ctx = requireActivity().getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        MapAPI mapAPI = new MapAPI();
+        map = root.findViewById(R.id.map);
+
+        //Request Permissions necessary for map to function.
+        String [] Permissions = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        requestPermissionsIfNecessary(Permissions);
+        map = mapAPI.buildMap(map, getContext());
+        map.getController().setCenter(new GeoPoint(Config.locationAPI.latitude, Config.locationAPI.longitude));
+
+        //allow user to add pins
+        MapEventsReceiver mReceive = new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                if(lookup.contains(p)) {
+                    System.out.println(p + " already has a pin");
+                    return false;
+                }
+                Marker startMarker = new Marker(map);
+                startMarker.setPosition(p);
+                startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+                map.getOverlays().add(startMarker);
+                lookup.add(p);
+                map.getController().setCenter(map.getMapCenter());
+                return true;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                return false;
+            }
+        };
+
+
+        MapEventsOverlay OverlayEvents = new MapEventsOverlay(getContext(), mReceive);
+        map.getOverlays().add(OverlayEvents);
+
+
+        return root;
     }
 
     @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // This will refresh the osmdroid configuration on resuming.
+        map.onPause();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, int[] grantResults) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>(Arrays.asList(permissions).subList(0, grantResults.length));
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    private void requestPermissionsIfNecessary(String[] permissions) {
+        ArrayList<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(requireActivity(), permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+    @Override
+    public boolean singleTapConfirmedHelper(GeoPoint p) {
+        IMapController mapController = map.getController();
+        mapController.animateTo(p);
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(p);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+        map.getOverlays().add(startMarker);
+        System.out.println(p);
+        return true;
+    }
+
+    @Override
+    public boolean longPressHelper(GeoPoint p) {
+        return false;
+    }
+
+
+    /*@Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         NewTreeViewModel mViewModel = new ViewModelProvider(this).get(NewTreeViewModel.class);
-    }
+    }*/
 
 }
