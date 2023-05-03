a5ec931df3f1909450a8819cd480321cc80464b5
Kaleb MacArthur <kaluubs@gmail.com>
2023-05-03 09:58:20 -0400

    Start statistics fragment.

--- app/src/main/java/com/ocdsb/mapletracker/ui/statistics/StatisticsFragment.java
+++ app/src/main/java/com/ocdsb/mapletracker/ui/statistics/StatisticsFragment.java
@@ -8,9 +8,12 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.ocdsb.mapletracker.api.MapAPI;
+import com.ocdsb.mapletracker.data.TreePin;
 import com.ocdsb.mapletracker.databinding.FragmentStatisticsBinding;
+
+import java.util.ArrayList;
 
 public class StatisticsFragment extends Fragment {
 
@@ -18,14 +21,41 @@ public class StatisticsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        StatisticsViewModel statisticsViewModel =
-                new ViewModelProvider(this).get(StatisticsViewModel.class);
-
         binding = FragmentStatisticsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        MapAPI mapAPI = new MapAPI();
+        mapAPI.loadPins(requireContext());
+        ArrayList<String> statistics = new ArrayList<>();
+
+        int treeCount = mapAPI.treePins.toArray().length;
+        double totalSapCollected = 0;
+        double resettableSapCollected = 0;
+        int totalEdits = 0;
+        int resettableEdits = 0;
+
+        for (TreePin tree: mapAPI.treePins) {
+            totalSapCollected += tree.sapLitresCollectedTotal;
+            resettableSapCollected += tree.sapLitresCollectedResettable;
+            totalEdits += tree.editsTotal;
+            resettableEdits += tree.editsResettable;
+        }
+
+        statistics.add("Tree count: " + treeCount);
+        statistics.add("Total sap collected: " + totalSapCollected);
+        statistics.add("Average sap per tree: " + totalSapCollected/treeCount);
+        statistics.add("Sap collected this season: " + resettableSapCollected);
+        statistics.add("Average tree yield this season: " + resettableSapCollected/treeCount);
+        statistics.add("Average total maple syrup yield: " + totalSapCollected/treeCount/40);
+        statistics.add("Estimated total maple syrup yield: " + totalSapCollected/40);
+        statistics.add("Estimated season maple yield: " + resettableSapCollected/40);
+        statistics.add("Estimated maple syrup per tree: " + resettableSapCollected/treeCount/40);
+        statistics.add("Taps logged in total: " + totalEdits);
+        statistics.add("Taps logged this season: " + resettableEdits);
+
         final TextView textView = binding.textStatistics;
-        statisticsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        textView.setText(String.join("\n• ", statistics));
+
         return root;
     }
 
