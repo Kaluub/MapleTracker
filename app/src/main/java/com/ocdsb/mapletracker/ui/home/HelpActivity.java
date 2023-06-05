c8bbfd54235d47dda790559923d5d823b8a94c2c
Kaleb MacArthur <kaluubs@gmail.com>
2023-06-05 12:34:32 -0400

    Help menu started

---
+++ app/src/main/java/com/ocdsb/mapletracker/ui/home/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.ocdsb.mapletracker.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ocdsb.mapletracker.R;
+
+public class HelpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
